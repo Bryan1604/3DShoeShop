05a722da2f6b116e79676f2c24073567462c8e52
Bryan1604 <luongvuhs@gmail.com>
2023-12-27 15:32:58 +0700

    Initial Commit

---
+++ ShoeApp/ShoeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoeAppApp.swift
+//  ShoeApp
+//
+//  Created by Luong Vu on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct ShoeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
